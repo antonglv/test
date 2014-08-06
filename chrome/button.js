96f10d5ca99ca92c4f0add57591a7b3930f38f43
Infocatcher <infocatcher.dev.null@gmail.com>
2014-08-06 22:49:33 +0400

    Detect generators in Firefox 2.0 - 4.0 (missing Function.prototype.isGenerator())

--- chrome/button.js
+++ chrome/button.js
@@ -1,2 +1,2 @@
-argNames += ",fn"; var fn = new Function (argNames, code); args. push (fn); var gen = fn. apply (oButton, args); if (fn. isGenerator && fn. isGenerator ()) { fn. __generator = gen; gen. next (); }
+argNames += ",fn"; var fn = new Function (argNames, code); args. push (fn); var gen = fn. apply (oButton, args); if ("isGenerator" in fn ? fn. isGenerator () : gen && typeof gen.next == "function") { fn. __generator = gen; gen. next (); }
 // Above code should be in one line for correct error line numbers
